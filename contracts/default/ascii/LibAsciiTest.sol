ba3f705fec50af2218cd3efc9abfa2156f2b21b3
longJJ2016 <1017303686@qq.com>
2021-07-22 10:04:13 +0800

    Ascii工具类

--- contracts/default/ascii/LibAsciiTest.sol
+++ contracts/default/ascii/LibAsciiTest.sol
@@ -4,13 +4,17 @@ import "./LibAscii.sol";
 
 contract LibAsciiTest{
 
-    function str2ascii() public {
+    function str2ascii() public view returns(string,string){
         string memory result= LibAscii.str2ascii("55",10);
         //result："7"
         string memory result1= LibAscii.str2ascii("55",16);
         //result1："U"
+        return (result,result1);
     }
-    function ascii2str(string ascii) public pure returns (string){
-        return LibAscii.ascii2str(ascii);
+
+    function ascii2str() public view returns(string){
+        string memory result=LibAscii.ascii2str("Aa");
+        //result："6597"
+        return result;
     }
 }
