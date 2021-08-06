866e0795a196b1f4c4edd59e8099b3a68017e02c
yanrangnan <yanrangnan@abakusglobal.com>
2021-08-06 18:30:02 +0800

    修改内置函数

--- contracts/internal_function/InternalFunction.sol
+++ contracts/internal_function/InternalFunction.sol
@@ -15,30 +15,6 @@ contract InternalFunction{
     }
 
     /**
-     * @notice 获取当前区块的矿工地址
-     * @return address 矿工地址
-     */
-    function getMiner() view public returns(address){
-        return block.coinbase;
-    }
-    
-    /**
-     * @notice 获取当前区块的难度系数
-     * @return uint256 难度系数
-     */
-    function getDifficulty() view public returns(uint256){
-        return block.difficulty;
-    }
-
-    /**
-     * @notice 获取当前区块的gasLimit
-     * @return uint256 gasLimit
-     */
-    function getGaslimit() view public returns(uint256){
-        return block.gaslimit;
-    }
-    
-    /**
      * @notice 获取当前区块的区块高度
      * @return uint256 区块高度
      */
@@ -55,19 +31,52 @@ contract InternalFunction{
     }
     
     /**
-     * @notice 获取当前区块的gasPrice
-     * @return uint256 gasPrice
+     *  @notice 判断地址是否是合约地址
      */
-    function getGasprice() view public returns(uint256) {
-        return tx.gasprice;
+    function isContract(address addr) view public returns (bool){
+        uint256 size;
+        assembly {
+            size := extcodesize(addr)
+        }
+        return size > 0;
     }
-    
+
     /**
-     * @notice 获取某个账户的eth余额
-     * @param  address account 待查询地址
-     * @return uint256 eth余额
+     * @notice 获取合约的代码节码
      */
-    function getEthBalance(address payable account) view public returns(uint256) {
-        return account.balance;
+    function getCodeByAddress(address addr) public view returns (bytes memory) {
+        require(isContract(addr) , "addr is not a contract");
+        bytes memory o_code;
+        assembly {
+            // 获取代码大小，这需要汇编语言
+            let size := extcodesize(addr)
+            // 分配输出字节数组 – 这也可以不用汇编语言来实现
+            // 通过使用 o_code = new bytes（size）
+            o_code := mload(0x40)
+            // 包括补位在内新的“memory end”
+            mstore(0x40, add(o_code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+            // 把长度保存到内存中
+            mstore(o_code, size)
+            // 实际获取代码，这需要汇编语言
+            extcodecopy(addr, add(o_code, 0x20), 0, size)
+        }
+        return o_code;
     }
-}
+
+    /**
+     * @notice 根据字节码计算合约地址
+     */
+    function computeAddress(bytes32 salt, bytes32 bytecodeHash) public view returns (address) {
+        return computeAddress(salt, bytecodeHash, address(this));
+    }
+
+    function computeAddress(
+        bytes32 salt,
+        bytes32 bytecodeHash,
+        address deployer
+    ) internal pure returns (address) {
+        bytes32 _data = keccak256(abi.encodePacked(bytes1(0xff), deployer, salt, bytecodeHash));
+        return address(uint160(uint256(_data)));
+    }
+
+}   
--- docs/internal_funtction/internalFunction.md
+++ docs/internal_funtction/internalFunction.md
@@ -6,10 +6,10 @@
 编号 | API | API描述
 ---|---|---
 1 | *getBlockhash(uint256 blockNumber) view public returns(bytes32)* | 根据指定的区块号获取hash(仅支持最近256个区块，且不包含当前区块)
-2 | *getMiner() view public returns(address)* |获取当前区块的矿工地址
-3 | *getDifficulty() view public returns(uint256) |获取当前区块的难度系数
-4 | *igetGaslimit() view public returns(uint256)* | 获取当前区块的区块高度
-5 | *getTimestamp() view public returns(uint256)* | 获取当前区块的时间戳
-6 | *getGasprice() view public returns(uint256)* | 获取当前区块的gasPrice
-7 | *getEthBalance(address payable account) view public returns(uint256)* | 获取某个账户的eth余额
+2 | *getBlockNumber() view public returns(address)* |获取当前区块的高度
+3 | *getTimestamp() view public returns(uint256)* | 获取当前区块的时间戳
+4 | *isContract(address addr) view public returns(bool)* | 判断一个地址是否是合约地址
+5 | *getCodeByAddress(address addr) view public returns(bytes)* | 根据合约地址获取合约代码
+6 | *computeAddress(bytes32 salt, bytes32 bytecodeHash) view public returns(address)* | 根据字节码计算合约的地址
+
 
