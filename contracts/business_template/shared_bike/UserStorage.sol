2ae068a80b8eca9410e698104e2690eb9669ecbe
aoronchu <aoronchu@gmail.com>
2022-03-02 11:51:20 +0800

    userstorage

--- contracts/business_template/shared_bike/UserStorage.sol
+++ contracts/business_template/shared_bike/UserStorage.sol
@@ -1,131 +1,59 @@
 pragma solidity ^0.6.10;
 
-import './UserStorage.sol';
+contract UserStorage{
 
-contract SharedBikes {
-
-    uint32 public userCreditThreshold = 60;
     address public admin;
-    UserStorage public userStorage;
 
-    constructor() public{
-        admin = msg.sender;
-        userStorage = new UserStorage(address(this));
-    }   
+    struct User {
+        string name;
+        string contact;
+        uint32 creditPoints;
+        uint32 status;
+    }
+
+    mapping(address=>User) private registeredUsers;
+
+    constructor(address _admin) public{
+        admin = _admin;
+    }
 
     modifier onlyAdmin(){
         require(msg.sender == admin, "admin required");
         _;
     }
 
-    modifier userRegistered(address user){
-        require(userStorage.exists(user), "user not registered");
-        _;
+    event RegisterUser(address indexed addr, string name, string contact);
+
+    function registerUser(string memory name, string memory contact) external {
+        require(!exists(msg.sender), "user existed");
+        User storage user = registeredUsers[msg.sender];
+        user.name = name;
+        user.contact = contact;
+        emit RegisterUser(msg.sender, name, contact);
     }
 
-    modifier requireStatus(uint256 bikeId, BikeStatus bikeStatus){
-        require(_bikes[bikeId].status == bikeStatus, "bad bike status");
-        _;
+    function getCredits(address user) external view returns(uint32 credits) { 
+        require(exists(user), "user not exists");       
+        return registeredUsers[user].creditPoints;
     }
 
-
-    struct Bike {
-        BikeStatus status;
+    function exists(address user) public view returns(bool) {
+        return registeredUsers[user].status != 0;
     }
 
-    enum BikeStatus {
-        NotExist,
-        Available,
-        InBorrow,
-        InRepair
+    function addCredits(address user, uint32 credits) public onlyAdmin {
+        require(credits > 0, "credits zero");
+        require(exists(user), "user not exists");   
+        uint32 newPoint = registeredUsers[user].creditPoints + credits;
+        require(newPoint > credits, "overflow");
+        registeredUsers[user].creditPoints= newPoint;
     }
 
-    event RegisterBike(uint256 indexed bikeId);
-    event RevokeBike(uint256 indexed bikeId);
-    event BorrowBike(address indexed borrower, uint256 indexed bikeId);
-    event ReturnBike(address indexed borrower, uint256 indexed bikeId);
-    event ReportDamage(address indexed reporter, uint256 indexed bikeId);
-    event FixDamage(uint256 indexed bikeId);
-    event Reward(address indexed borrower, uint32 credits);
-    event Punish(address indexed borrower, uint32 credits);
-    event SetCreditThreshold(uint32 newThreshold);
-    event TransferAdmin(address oldAdmin, address newAdmin);
-
-    uint256 private _bikeCount;
-    mapping(uint256=>Bike) private _bikes;
-    mapping(address=>uint256) private _borrows;
-
-    // Business functions
-    function registerBike() external onlyAdmin returns(uint256 bikeId) {
-        _bikeCount++;
-        bikeId = _bikeCount;
-        Bike storage bike = _bikes[bikeId];
-        bike.status = BikeStatus.Available;
-        emit RegisterBike(bikeId);
-    }
-
-    function revokeBike(uint256 bikeId) external onlyAdmin requireStatus(bikeId, BikeStatus.Available){
-        Bike storage bike = _bikes[bikeId];
-        bike.status = BikeStatus.NotExist;
-        emit RevokeBike(bikeId);
-    }
-
-    function borrowBike(uint256 bikeId) external userRegistered(msg.sender) requireStatus(bikeId, BikeStatus.Available){
-        require(_borrows[msg.sender] == 0, "user in borrow");
-        require(userStorage.getCredits(msg.sender) >= userCreditThreshold, "not enough credits");
-        _borrows[msg.sender] = bikeId;
-        emit BorrowBike(msg.sender, bikeId);
-    }
-
-    function returnBike(uint256 bikeId) external  userRegistered(msg.sender) requireStatus(bikeId, BikeStatus.InBorrow){
-        require(_borrows[msg.sender] == bikeId, "not borrowing this one");
-        _borrows[msg.sender] = 0;
-        Bike storage bike = _bikes[bikeId];
-        bike.status = BikeStatus.Available;
-        emit ReturnBike(msg.sender, bikeId);
-    }
-
-    function reportDamge(uint256 bikeId) external  userRegistered(msg.sender) requireStatus(bikeId, BikeStatus.Available){
-        Bike storage bike = _bikes[bikeId];
-        bike.status = BikeStatus.InRepair;
-        emit ReportDamage(msg.sender, bikeId);
-    }
-
-    function fixDamge(uint256 bikeId) external onlyAdmin requireStatus(bikeId, BikeStatus.InRepair){
-        Bike storage bike = _bikes[bikeId];
-        bike.status = BikeStatus.InRepair;
-    }
-
-    function reward(address user, uint32 credits) external userRegistered(user) onlyAdmin{
-        userStorage.addCredits(user, credits);
-        emit Reward(user, credits);
-    }
-
-    function punish(address user, uint32 credits) external userRegistered(user) onlyAdmin{
-        userStorage.subCredits(user, credits);
-        emit Punish(user, credits);
-    }
-
-    // Governance functions
-    function setCreditThreshold(uint32 newThreshold) external onlyAdmin {
-        userCreditThreshold = newThreshold;
-        emit SetCreditThreshold(newThreshold);
-    }
-
-    function transferAdmin(address newAdmin) external onlyAdmin {
-        address oldAdmin = admin;
-        admin = newAdmin;
-        emit TransferAdmin(oldAdmin, newAdmin);
+    function subCredits(address user, uint32 credits) public onlyAdmin {
+        require(credits > 0, "credits zero");
+        require(exists(user), "user not exists");   
+        uint32 newPoint = registeredUsers[user].creditPoints - credits;
+        require(newPoint < credits, "overflow");
+        registeredUsers[user].creditPoints= newPoint;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
