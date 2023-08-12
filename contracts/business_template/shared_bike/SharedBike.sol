92361b5360a99adb6d8878475d3b33c167211dde
tttwei <2119515837@qq.com>
2023-08-12 17:44:37 +0800

    fix bug

--- contracts/business_template/shared_bike/SharedBike.sol
+++ contracts/business_template/shared_bike/SharedBike.sol
@@ -2,15 +2,21 @@ pragma solidity ^0.6.10;
 
 import './UserStorage.sol';
 
+
 contract SharedBikes {
 
     uint32 public userCreditThreshold = 60;
     address public admin;
     UserStorage public userStorage;
 
-    constructor() public{
+   
+
+    //使用合约地址构造，指定要获取的对象
+    constructor(address _address) public{
         admin = msg.sender;
-        userStorage = new UserStorage(address(this));
+        address addr=address(_address);
+        userStorage =UserStorage(addr);
+       
     }   
 
     modifier onlyAdmin(){
@@ -28,11 +34,16 @@ contract SharedBikes {
         _;
     }
 
-
+/**
+ * 定义单车状态对象
+ */
     struct Bike {
         BikeStatus status;
     }
-
+    /**
+     * 定义一个枚举，以便可以使用数字代表单车的状态
+     * 不存在 0 ,可使用 1 ,已借出 2 ,修理中 3
+     */
     enum BikeStatus {
         NotExist,
         Available,
@@ -40,22 +51,27 @@ contract SharedBikes {
         InRepair
     }
 
-    event RegisterBike(uint256 indexed bikeId);
-    event RevokeBike(uint256 indexed bikeId);
-    event BorrowBike(address indexed borrower, uint256 indexed bikeId);
-    event ReturnBike(address indexed borrower, uint256 indexed bikeId);
-    event ReportDamage(address indexed reporter, uint256 indexed bikeId);
-    event FixDamage(uint256 indexed bikeId);
-    event Reward(address indexed borrower, uint32 credits);
-    event Punish(address indexed borrower, uint32 credits);
-    event SetCreditThreshold(uint32 newThreshold);
-    event TransferAdmin(address oldAdmin, address newAdmin);
-
+    
+    event RegisterBike(uint256 indexed bikeId);//注册
+    event RevokeBike(uint256 indexed bikeId);//废除
+    event BorrowBike(address indexed borrower, uint256 indexed bikeId);//借出
+    event ReturnBike(address indexed borrower, uint256 indexed bikeId);//归还
+    event ReportDamage(address indexed reporter, uint256 indexed bikeId);//报告损坏
+    event FixDamage(uint256 indexed bikeId);//修复损坏
+    event Reward(address indexed borrower, uint32 credits);//奖励
+    event Punish(address indexed borrower, uint32 credits);//惩罚
+    event SetCreditThreshold(uint32 newThreshold);//设置信用阈值
+    event TransferAdmin(address oldAdmin, address newAdmin);//转移使用者
+    
+    //自增单车ID
     uint256 private _bikeCount;
+    //存储不同单车的状态
     mapping(uint256=>Bike) private _bikes;
+    //存储用户借用
     mapping(address=>uint256) private _borrows;
 
     // Business functions
+    //增
     function registerBike() external onlyAdmin returns(uint256 bikeId) {
         _bikeCount++;
         bikeId = _bikeCount;
@@ -64,19 +80,39 @@ contract SharedBikes {
         emit RegisterBike(bikeId);
     }
 
+//  function getUser() external returns (string memory, string memory, uint32, uint32) {
+//     require(userStorage.exists(msg.sender), "user not exists");
+//     (string memory name, string memory contact, uint32 creditPoints, uint32 status) = userStorage.getUser();
+//     return (name, contact, creditPoints, status);
+// }
+//  function ex() external view returns(bool){
+//     bool b= userStorage.exists(msg.sender);
+//     return b;
+//  }
+
+    //删
     function revokeBike(uint256 bikeId) external onlyAdmin requireStatus(bikeId, BikeStatus.Available){
         Bike storage bike = _bikes[bikeId];
+        //修改单车状态为不存在
         bike.status = BikeStatus.NotExist;
         emit RevokeBike(bikeId);
     }
-
+    //借
     function borrowBike(uint256 bikeId) external userRegistered(msg.sender) requireStatus(bikeId, BikeStatus.Available){
+        //判断是否已经借出
         require(_borrows[msg.sender] == 0, "user in borrow");
+        //判断用户信用分数
         require(userStorage.getCredits(msg.sender) >= userCreditThreshold, "not enough credits");
+
+        //修改借用者状态
         _borrows[msg.sender] = bikeId;
+        //修改自行车状态
+        Bike storage bike = _bikes[bikeId];
+        bike.status = BikeStatus.InBorrow;
+
         emit BorrowBike(msg.sender, bikeId);
     }
-
+    //还
     function returnBike(uint256 bikeId) external  userRegistered(msg.sender) requireStatus(bikeId, BikeStatus.InBorrow){
         require(_borrows[msg.sender] == bikeId, "not borrowing this one");
         _borrows[msg.sender] = 0;
@@ -93,9 +129,10 @@ contract SharedBikes {
 
     function fixDamge(uint256 bikeId) external onlyAdmin requireStatus(bikeId, BikeStatus.InRepair){
         Bike storage bike = _bikes[bikeId];
-        bike.status = BikeStatus.InRepair;
+        bike.status = BikeStatus.Available;
+        emit FixDamage(bikeId);
     }
-
+    
     function reward(address user, uint32 credits) external userRegistered(user) onlyAdmin{
         userStorage.addCredits(user, credits);
         emit Reward(user, credits);
--- contracts/business_template/shared_bike/UserStorage.sol
+++ contracts/business_template/shared_bike/UserStorage.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.6.10;
 
+
+
 contract UserStorage{
 
     address public admin;
+    
 
     struct User {
         string name;
@@ -11,25 +14,40 @@ contract UserStorage{
         uint32 status;
     }
 
-    mapping(address=>User) private registeredUsers;
+    User public users;
 
-    constructor(address _admin) public{
-        admin = _admin;
-    }
+    mapping(address=>User) public registeredUsers;
 
-    modifier onlyAdmin(){
-        require(msg.sender == admin, "admin required");
-        _;
+    //直接用调用者作为admin地址
+    constructor() public{
+        admin = msg.sender;
     }
+    //由于通过SharedBike来调用的合约，获取到的msg.sender为调用合约的地址并非SharedBike的调用者
+    // modifier onlyAdmin(){
+    //     require(msg.sender== admin , "uadmin required");
+    //     _;
+    // }
 
     event RegisterUser(address indexed addr, string name, string contact);
 
+
+  
+
     function registerUser(string memory name, string memory contact) external {
+        //if status==0,is new User
         require(!exists(msg.sender), "user existed");
-        User storage user = registeredUsers[msg.sender];
-        user.name = name;
-        user.contact = contact;
-        user.status = 1;
+        // User storage user = registeredUsers[msg.sender];
+        // user.name = name;
+        // user.contact = contact;
+        registeredUsers[msg.sender].name = name;
+        registeredUsers[msg.sender].contact = contact;
+        registeredUsers[msg.sender].status = 1;
+        registeredUsers[msg.sender].creditPoints = 100;
+        
+        
+       
+        
+
         emit RegisterUser(msg.sender, name, contact);
     }
 
@@ -39,22 +57,32 @@ contract UserStorage{
     }
 
     function exists(address user) public view returns(bool) {
+        // bool b=(registeredUsers[user].status != 0);
         return registeredUsers[user].status != 0;
     }
 
-    function addCredits(address user, uint32 credits) public onlyAdmin {
-        require(credits > 0, "credits zero");
+    function addCredits(address user, uint32 credits) public  {
+        
         require(exists(user), "user not exists");   
+        // 0信誉就不能加分了吗
+        // require(credits > 0, "credits zero");
         uint32 newPoint = registeredUsers[user].creditPoints + credits;
-        require(newPoint > credits, "overflow");
+        // require(newPoint >= credits, "overflow");
         registeredUsers[user].creditPoints= newPoint;
     }
 
-    function subCredits(address user, uint32 credits) public onlyAdmin {
-        require(credits > 0, "credits zero");
+    function subCredits(address user, uint32 credits) public  {
         require(exists(user), "user not exists");   
-        uint32 newPoint = registeredUsers[user].creditPoints - credits;
-        require(newPoint < credits, "overflow");
+        require(credits > 0, "credits zero");
+        //添加if判断，防止溢出
+        uint32 newPoint=0;
+        if (registeredUsers[user].creditPoints > credits) {
+            newPoint = registeredUsers[user].creditPoints - credits;
+        } else {
+            newPoint = 0;
+        }
+         
+        // require(newPoint <= credits, "overflow");错误的比较100-10=90，90>10不成立，无意义的比较
         registeredUsers[user].creditPoints= newPoint;
     }
 }
