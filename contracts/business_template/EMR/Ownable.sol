3d36984b0a1f81265065e51af9d5d56fc0cbc55b
userInner <1239989762@qq.com>
2023-08-18 16:33:15 +0800

    添加电子病历相关合约示例

---
+++ contracts/business_template/EMR/Ownable.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.25;
+
+
+contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor (address owner) internal {
+        _owner = owner;
+        emit OwnershipTransferred(address(0), owner);
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        _owner = newOwner;
+    }
+
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
