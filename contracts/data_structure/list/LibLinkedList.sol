654d604c4c93b14be6d07347ba00c294ac2afeeb
yuanjiaja <136324757+yuanjiaja@users.noreply.github.com>
2023-10-16 14:25:28 +0800

    tsk 52对智能合约中对heap和list进行代码注释 (#145)
    对智能合约中对heap和list进行代码注释
    
    Co-authored-by: dalaocu <njumjy06@126.com>

--- contracts/data_structure/list/LibLinkedList.sol
+++ contracts/data_structure/list/LibLinkedList.sol
@@ -16,111 +16,107 @@
 
 pragma solidity ^0.4.25;
 
-
 library LibLinkedList {
-    
-    bytes32 constant private NULL = bytes32(0);
-    bool constant private PREV = false;
-    bool constant private NEXT = true;
 
-    struct LinkedList{
-        uint256 size;
-        bytes32 head;
-        bytes32 tail;
-        mapping(bytes32 => Node) indexs;
+    bytes32 constant private NULL = bytes32(0); // 定义一个常量NULL，用于表示空节点
+    bool constant private PREV = false; // 用于指示前一个节点
+    bool constant private NEXT = true; // 用于指示后一个节点
+
+    struct LinkedList {
+        uint256 size; // 记录链表的大小
+        bytes32 head; // 链表的头节点
+        bytes32 tail; // 链表的尾节点
+        mapping(bytes32 => Node) indexs; // 通过节点的值映射到节点对象的索引
     }
 
-    struct Node{
-        bytes32 prev;
-        bytes32 next;
-        bool exist;
+    struct Node {
+        bytes32 prev; // 前一个节点的值
+        bytes32 next; // 后一个节点的值
+        bool exist; // 标记节点是否存在
     }
 
-    function getSize(LinkedList storage self) internal view returns(uint256){
+    function getSize(LinkedList storage self) internal view returns (uint256) {
         return self.size;
     }
 
-    function addNode(LinkedList storage self, bytes32 data) internal returns(LinkedList storage) {
-        require(data != bytes32(0));
-        require(!self.indexs[data].exist);
-        if(self.size == 0){
-            Node memory node = Node(NULL, NULL, true);
-            self.size = 1;
-            self.head = data;
-            self.tail = data;
-            self.indexs[data] = node;
-        }
-        else{
-            Node memory newNode = Node(self.tail, NULL, true); 
-            Node storage tail = extractNode(self, self.tail);
-            tail.next = data;
-            self.tail = data;
-            self.size++;
-            self.indexs[data] = newNode;
+    function addNode(LinkedList storage self, bytes32 data) internal returns (LinkedList storage) {
+        require(data != bytes32(0)); // 确保添加的节点值不为空
+        require(!self.indexs[data].exist); // 确保节点值在链表中不存在
+        if (self.size == 0) {
+            Node memory node = Node(NULL, NULL, true); // 创建一个新的节点对象
+            self.size = 1; // 更新链表大小为1
+            self.head = data; // 设置链表的头节点为新添加的节点
+            self.tail = data; // 设置链表的尾节点为新添加的节点
+            self.indexs[data] = node; // 在索引中设置新节点
+        } else {
+            Node memory newNode = Node(self.tail, NULL, true); // 创建一个新的节点对象
+            Node storage tail = extractNode(self, self.tail); // 获取尾节点
+            tail.next = data; // 尾节点的下一个节点是新添加的节点
+            self.tail = data; // 更新链表的尾节点为新添加的节点
+            self.size++; // 更新链表的大小
+            self.indexs[data] = newNode; // 在索引中设置新节点
         }
         return self;
     }
 
-    function getPrev(LinkedList storage self, bytes32 data) internal view returns(bytes32){
-        Node storage node = self.indexs[data];
-        require(node.exist);
-        return node.prev;
+    function getPrev(LinkedList storage self, bytes32 data) internal view returns (bytes32) {
+        Node storage node = self.indexs[data]; // 获取节点对象
+        require(node.exist); // 确保节点存在
+        return node.prev; // 返回前一个节点的值
     }
 
-    function getNext(LinkedList storage self, bytes32 data) internal view returns(bytes32){
-        Node storage node = self.indexs[data];
-        require(node.exist);
-        return node.next;
+    function getNext(LinkedList storage self, bytes32 data) internal view returns (bytes32) {
+        Node storage node = self.indexs[data]; // 获取节点对象
+        require(node.exist); // 确保节点存在
+        return node.next; // 返回后一个节点的值
     }
 
-    
-    function removeNode(LinkedList storage self, bytes32 data) internal returns(LinkedList storage){
-        Node storage node = extractNode(self, data);
-        require(node.exist);
-        Node storage prev = extractNode(self, node.prev);
-        Node storage next = extractNode(self, node.next);
-        if(prev.exist){
-            prev.next = node.next;
-        }
-        else{
-            self.head = node.next;
+
+    function removeNode(LinkedList storage self, bytes32 data) internal returns (LinkedList storage) {
+        Node storage node = extractNode(self, data); // 获取节点对象
+        require(node.exist);  //确保节点存在
+        Node storage prev = extractNode(self, node.prev);  //获取前一个节点对象
+        Node storage next = extractNode(self, node.next);  //获取后一个节点对象
+        if (prev.exist) {
+            prev.next = node.next;  //前一个节点的下一个节点更新为当前节点的下一个节点
+        } else {
+            self.head = node.next;  //更新链表的头节点为当前节点的下一个节点
         }
 
-        if(next.exist){
-            next.prev = node.prev;
+        if (next.exist) {
+            next.prev = node.prev;  //后一个节点的前一个节点更新为当前节点的前一个节点
+        } else {
+            self.tail = node.prev;  //更新链表的尾节点为当前节点的前一个节点
         }
-        else{
-            self.tail = node.prev;
-        }
-        delete self.indexs[data];
-        self.size--;
+        delete self.indexs[data];  // 从索引中删除节点
+        self.size--; //更新链表的大小
         return self;
     }
-    
-      
-    function getTail(LinkedList storage self) internal view returns(bytes32){
-        return self.tail;
+
+
+    function getTail(LinkedList storage self) internal view returns (bytes32) {
+        return self.tail; //返回链表的尾节点值
     }
 
-    function getHead(LinkedList storage self) internal view returns(bytes32){
-        return self.head;
-    }  
-    
-    function extractNode(LinkedList storage self, bytes32 data) private view returns(Node storage){
-        return self.indexs[data];
+    function getHead(LinkedList storage self) internal view returns (bytes32) {
+        return self.head; //返回链表的头节点值
     }
 
-    
+    function extractNode(LinkedList storage self, bytes32 data) private view returns (Node storage) {
+        return self.indexs[data]; //获取索引中对应节点的对象
+    }
+
+
     // -----------Iterative functions------------------
-    function iterate_start(LinkedList storage self) internal view returns (bytes32){
-        return self.head;
+    function iterate_start(LinkedList storage self) internal view returns (bytes32) {
+        return self.head; //返回链表的头节点值作为迭代的起始点
     }
 
-    function can_iterate(LinkedList storage self, bytes32 data) internal view returns(bool){
-        return self.indexs[data].exist;
+    function can_iterate(LinkedList storage self, bytes32 data) internal view returns (bool) {
+        return self.indexs[data].exist; //判断节点是否存在
     }
 
-    function iterate_next(LinkedList storage self, bytes32 data) internal view returns(bytes32){
-        return self.indexs[data].next;
+    function iterate_next(LinkedList storage self, bytes32 data) internal view returns (bytes32) {
+        return self.indexs[data].next; //返回当前节点的下一个节点
     }
 }
--- contracts/data_structure/list/LibSingleList.sol
+++ contracts/data_structure/list/LibSingleList.sol
@@ -1,150 +1,181 @@
  pragma solidity ^0.4.25;
- 
- library LibSingleList {
-     bytes32 constant private NULL = bytes32(0);
-     struct ListNode{
-         bytes32 nextNode;
-         bool exist;
-     }
-     struct List{
-        mapping(bytes32 => ListNode) data;
-        bytes32 header;
-        bytes32 tail;
-        uint256 size;
+library LibSingleList {
+    // 定义常量NULL，初始值为bytes32类型的0
+    bytes32 constant private NULL = bytes32(0);
+
+    // 定义单链表的节点结构体
+    struct ListNode {
+        bytes32 nextNode; // 下一个节点的指针
+        bool exist; // 节点是否存在的标志
     }
 
-    struct Iterate{
-        bytes32 value;
-        bytes32 prevNode;
+    // 定义单链表结构体
+    struct List {
+        mapping(bytes32 => ListNode) data; // 存储链表节点的映射表
+        bytes32 header; // 链表头节点的指针
+        bytes32 tail; // 链表尾节点的指针
+        uint256 size; // 链表的大小
     }
-    
+
+    // 迭代器结构体，用于遍历链表
+    struct Iterate {
+        bytes32 value; // 当前节点的指针
+        bytes32 prevNode; // 前一个节点的指针
+    }
+
+    // 在链表尾部插入一个元素
     function pushBack(List storage self, bytes32 ele) internal {
+        // 判断元素是否已存在
         require(!self.data[ele].exist, "the element is already exists");
-        if(self.size == 0){
+
+        // 若链表为空，则将元素设置为头节点
+        if (self.size == 0) {
             addFirstNode(self, ele);
         } else {
-            self.data[ele] = ListNode({nextNode:NULL, exist:true});
+            // 否则，在尾节点后插入新元素
+            self.data[ele] = ListNode({nextNode: NULL, exist: true});
             self.data[self.tail].nextNode = ele;
             self.tail = ele;
         }
 
         self.size++;
     }
-    
+
+    // 在链表头部插入一个元素
     function pushFront(List storage self, bytes32 ele) internal {
+        // 判断元素是否已存在
         require(!self.data[ele].exist, "the element is already exists");
-        if(self.size == 0){
-           addFirstNode(self, ele);
+
+        // 若链表为空，则将元素设置为头节点
+        if (self.size == 0) {
+            addFirstNode(self, ele);
         } else {
-            self.data[ele] = ListNode({nextNode:self.header, exist:true});
+            // 否则，在头节点前插入新元素
+            self.data[ele] = ListNode({nextNode: self.header, exist: true});
             self.header = ele;
         }
         self.size++;
     }
-    //删除一个元素 o(n)
-    function remove(List storage self, bytes32 ele) internal  {
+
+    // 删除一个元素
+    function remove(List storage self, bytes32 ele) internal {
         Iterate memory iter = find(self, ele);
-        if(iter.value != NULL){
+        if (iter.value != NULL) {
             remove(self, iter);
         }
     }
-    //查找 o(n)
-    function find(List storage self, bytes32 ele) internal returns(Iterate memory) {
-        require(self.data[ele].exist, "the node is not exists" ); 
+
+    // 查找一个元素
+    function find(List storage self, bytes32 ele) internal returns (Iterate memory) {
+        // 判断节点是否存在并返回对应的迭代器
+        require(self.data[ele].exist, "the node is not exists");
         bytes32 prev = NULL;
         bytes32 beg = self.header;
-        while(beg != NULL){
-            if(beg == ele){
-                Iterate memory iter = Iterate({value:ele, prevNode:prev});
+        while (beg != NULL) {
+            if (beg == ele) {
+                Iterate memory iter = Iterate({value: ele, prevNode: prev});
                 return iter;
             }
             prev = beg;
             beg = self.data[beg].nextNode;
         }
-        return Iterate({value:NULL, prevNode:NULL});
+        return Iterate({value: NULL, prevNode: NULL});
     }
 
-    function remove(List storage self, Iterate memory iter) internal returns(Iterate memory){
-        require(self.data[iter.value].exist, "the node is not exists" );
-        Iterate memory nextIter = Iterate({value:self.data[iter.value].nextNode, prevNode:iter.prevNode});
-        if(iter.prevNode == NULL) {
+    // 删除一个节点
+    function remove(List storage self, Iterate memory iter) internal returns (Iterate memory) {
+        require(self.data[iter.value].exist, "the node is not exists");
+        Iterate memory nextIter = Iterate({value: self.data[iter.value].nextNode, prevNode: iter.prevNode});
+        if (iter.prevNode == NULL) {
+            // 若删除的是头节点，则更新头指针
             self.header = self.data[iter.value].nextNode;
         } else {
+            // 否则，将前一个节点的next指针指向待删除节点的下一个节点
             self.data[iter.prevNode].nextNode = self.data[iter.value].nextNode;
         }
         delete self.data[iter.value];
-        if(self.header == NULL){
+        if (self.header == NULL) {
             self.tail = NULL;
         }
         self.size--;
         return nextIter;
     }
 
-    function insertNext(List storage self, Iterate memory iter, bytes32 ele)internal {
-        require(self.data[iter.value].exist, "the node is not exists" );
+    // 在某个节点之后插入一个元素
+    function insertNext(List storage self, Iterate memory iter, bytes32 ele) internal {
+        require(self.data[iter.value].exist, "the node is not exists");
         bytes32 next = self.data[iter.value].nextNode;
-        self.data[ele] = ListNode({nextNode:next, exist:true});
+        self.data[ele] = ListNode({nextNode: next, exist: true});
         self.data[iter.value].nextNode = ele;
-        if(self.tail == iter.value){
+        if (self.tail == iter.value) {
             self.tail = ele;
         }
         self.size++;
     }
 
-    function insertPrev(List storage self, Iterate memory iter, bytes32 ele) internal{
-        require(self.data[iter.value].exist, "the node is not exists" );
-        self.data[ele] = ListNode({nextNode:iter.value, exist:true});
-        if(self.header == iter.value){
+    // 在某个节点之前插入一个元素
+    function insertPrev(List storage self, Iterate memory iter, bytes32 ele) internal {
+        require(self.data[iter.value].exist, "the node is not exists");
+        self.data[ele] = ListNode({nextNode: iter.value, exist: true});
+        if (self.header == iter.value) {
             self.header = ele;
         } else {
-            require(self.data[iter.prevNode].exist, "the prev node is not exists" );
+            require(self.data[iter.prevNode].exist, "the prev node is not exists");
             self.data[iter.prevNode].nextNode = ele;
         }
-         self.size++;
+        self.size++;
     }
-    
-    function addFirstNode(List storage self, bytes32 ele)  private {
-        ListNode memory node = ListNode({nextNode:NULL, exist:true});
+
+    // 将元素作为第一个节点插入链表中
+    function addFirstNode(List storage self, bytes32 ele) private {
+        ListNode memory node = ListNode({nextNode: NULL, exist: true});
         self.data[ele] = node;
-        self.header =  ele;
+        self.header = ele;
         self.tail = ele;
     }
 
-    function getBack(List storage self) internal returns(bytes32) {
-        require(self.size > 0, "the list is empty" ); 
+    // 获取链表尾节点的元素
+    function getBack(List storage self) internal returns (bytes32) {
+        require(self.size > 0, "the list is empty");
         return self.tail;
     }
 
-    function getFront(List storage self) internal returns(bytes32) {
-        require(self.size > 0, "the list is empty" ); 
+    // 获取链表头节点的元素
+    function getFront(List storage self) internal returns (bytes32) {
+        require(self.size > 0, "the list is empty");
         return self.header;
     }
-    
-    function getSize(List storage self) internal view returns(uint256){
-       return self.size;
+
+    // 获取链表大小
+    function getSize(List storage self) internal view returns (uint256) {
+        return self.size;
     }
-    
-    function isEmpty(List storage self) internal view returns(bool){
+
+    // 判断链表是否为空
+    function isEmpty(List storage self) internal view returns (bool) {
         return self.size == 0;
     }
 
-    function isExists(List storage self, bytes32 ele) internal view returns(bool){
+    // 判断节点是否存在
+    function isExists(List storage self, bytes32 ele) internal view returns (bool) {
         return self.data[ele].exist;
     }
-    
-    function begin(List storage self) internal view returns(Iterate){
-        Iterate memory iter = Iterate({value:self.header, prevNode:NULL});
-        return iter;   
+
+    // 获取链表头节点的迭代器
+    function begin(List storage self) internal view returns (Iterate) {
+        Iterate memory iter = Iterate({value: self.header, prevNode: NULL});
+        return iter;
     }
-    
-    function isEnd(List storage self, Iterate memory iter) internal view returns(bool){
+
+    // 判断迭代器是否指向链表尾部
+    function isEnd(List storage self, Iterate memory iter) internal view returns (bool) {
         return iter.value == NULL;
     }
-    
-    function nextNode(List storage self, Iterate memory iter) internal view returns(Iterate){
-        require(self.data[iter.value].exist, "the node is not exists" );
-        Iterate memory nextIter = Iterate({value:self.data[iter.value].nextNode, prevNode:iter.value});
+
+    // 获取迭代器的下一个节点
+    function nextNode(List storage self, Iterate memory iter) internal view returns (Iterate) {
+        require(self.data[iter.value].exist, "the node is not exists");
+        Iterate memory nextIter = Iterate({value: self.data[iter.value].nextNode, prevNode: iter.value});
         return nextIter;
     }
-    
- }
+}
