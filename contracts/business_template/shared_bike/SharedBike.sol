75429d881a7fc264a0b99ccdcb62bad14399c618
dalaocu <njumjy06@126.com>
2023-10-29 19:16:09 +0800

    arrange files

--- contracts/business_template/shared_bike/SharedBike.sol
+++ contracts/business_template/shared_bike/SharedBike.sol
@@ -2,172 +2,189 @@ pragma solidity ^0.6.10;
 
 import './UserStorage.sol';
 
-
+// 共享单车主合约
 contract SharedBikes {
 
+    // 类似于用户的信誉阈值
     uint32 public userCreditThreshold = 60;
+    // 合约的调用者
     address public admin;
+    // 用户的数据存储
     UserStorage public userStorage;
 
-   
-
-    //使用合约地址构造，指定要获取的对象
-    constructor(address _address) public{
+    // 初始化构造函数
+    constructor() public{
         admin = msg.sender;
-        address addr=address(_address);
-        userStorage =UserStorage(addr);
-       
+        userStorage = new UserStorage(address(this));
     }   
-
+    // 修饰符判断当前只有管理员有权限
     modifier onlyAdmin(){
         require(msg.sender == admin, "admin required");
         _;
     }
-
+    
+    // 判断当前的用户是否已经注册
     modifier userRegistered(address user){
-        //用户是否存在
         require(userStorage.exists(user), "user not registered");
         _;
     }
 
+    // 判断共享单车的状态
     modifier requireStatus(uint256 bikeId, BikeStatus bikeStatus){
-        //判断单车状态
         require(_bikes[bikeId].status == bikeStatus, "bad bike status");
         _;
     }
 
-    /**
-     * 定义单车状态对象
-     */
+    // 存储单车的结构体
     struct Bike {
         BikeStatus status;
     }
-    /**
-     * 定义一个枚举，以便可以使用数字代表单车的状态
-     * 不存在 0 ,可使用 1 ,已借出 2 ,修理中 3
-     */
+
+    // 共享单车的状态
     enum BikeStatus {
         NotExist,
         Available,
         InBorrow,
         InRepair
     }
+    // 注册单车事件：当管理员成功注册一辆新单车时触发
+    event RegisterBike(uint256 indexed bikeId);
+    
+    // 注销单车事件：当管理员成功注销一辆单车时触发
+    event RevokeBike(uint256 indexed bikeId);
+    
+    // 借车事件：当用户成功借走一辆单车时触发
+    event BorrowBike(address indexed borrower, uint256 indexed bikeId);
+    
+    // 还车事件：当用户成功将借走的单车归还时触发
+    event ReturnBike(address indexed borrower, uint256 indexed bikeId);
+    
+    // 报告损坏事件：当用户报告单车损坏时触发
+    event ReportDamage(address indexed reporter, uint256 indexed bikeId);
+    
+    // 修复损坏事件：当管理员修复损坏的单车时触发
+    event FixDamage(uint256 indexed bikeId);
+    
+    // 奖励事件：当管理员对用户进行信用积分奖励时触发
+    event Reward(address indexed borrower, uint32 credits);
+    
+    // 惩罚事件：当管理员对用户进行信用积分惩罚时触发
+    event Punish(address indexed borrower, uint32 credits);
+    
+    // 设置信用积分阈值事件：当管理员成功设置新的信用积分阈值时触发
+    event SetCreditThreshold(uint32 newThreshold);
+    
+    // 转移管理员权限事件：当管理员权限成功转移时触发
+    event TransferAdmin(address oldAdmin, address newAdmin);
 
-    
-    event RegisterBike(uint256 indexed bikeId);//注册
-    event RevokeBike(uint256 indexed bikeId);//废除
-    event BorrowBike(address indexed borrower, uint256 indexed bikeId);//借出
-    event ReturnBike(address indexed borrower, uint256 indexed bikeId);//归还
-    event ReportDamage(address indexed reporter, uint256 indexed bikeId);//报告损坏
-    event FixDamage(uint256 indexed bikeId);//修复损坏
-    event Reward(address indexed borrower, uint32 credits);//奖励
-    event Punish(address indexed borrower, uint32 credits);//惩罚
-    event SetCreditThreshold(uint32 newThreshold);//设置信用阈值
-    event TransferAdmin(address oldAdmin, address newAdmin);//转移使用者
-    
-    //自增单车ID
+	// 共享单车的总数全局变量
     uint256 private _bikeCount;
-    //存储不同单车的状态
+    // ID对应Bike的结构体
     mapping(uint256=>Bike) private _bikes;
-    //存储用户借用
+    // 存储被用户租借的共享单车ID
     mapping(address=>uint256) private _borrows;
 
-    //增
+    /**
+     * 共享单车公司进行添加共享单车
+     */
     function registerBike() external onlyAdmin returns(uint256 bikeId) {
-        //自增Id
         _bikeCount++;
         bikeId = _bikeCount;
-        //修改自行车状态
         Bike storage bike = _bikes[bikeId];
         bike.status = BikeStatus.Available;
         emit RegisterBike(bikeId);
     }
 
-
-    //删
+    /**
+     * 注销单车
+     * @param bikeId 共享单车ID
+     */
     function revokeBike(uint256 bikeId) external onlyAdmin requireStatus(bikeId, BikeStatus.Available){
         Bike storage bike = _bikes[bikeId];
-        //修改单车状态为不存在
         bike.status = BikeStatus.NotExist;
         emit RevokeBike(bikeId);
     }
-    //借
+
+    /**
+     * 租借单车
+     * @param bikeId 共享单车ID
+     */
     function borrowBike(uint256 bikeId) external userRegistered(msg.sender) requireStatus(bikeId, BikeStatus.Available){
-        //判断是否已经借出
+    	// 判断当前的共享是否已经被租借
         require(_borrows[msg.sender] == 0, "user in borrow");
-        //判断用户信用分数
+        // 判断当前用户的积分是否大于或者等于预设的信用积分阈值
         require(userStorage.getCredits(msg.sender) >= userCreditThreshold, "not enough credits");
-
-        //修改借用者状态
         _borrows[msg.sender] = bikeId;
-        //修改自行车状态
-        Bike storage bike = _bikes[bikeId];
-        bike.status = BikeStatus.InBorrow;
-
         emit BorrowBike(msg.sender, bikeId);
     }
-    //还
+    
+    /**
+     * 归还单车
+     * @param bikeId 共享单车ID
+     */ 
     function returnBike(uint256 bikeId) external  userRegistered(msg.sender) requireStatus(bikeId, BikeStatus.InBorrow){
+    	// 判断当前的共享单车是否已经被借阅
         require(_borrows[msg.sender] == bikeId, "not borrowing this one");
-        //修改借用者状态
         _borrows[msg.sender] = 0;
-        //修改自行车状态
         Bike storage bike = _bikes[bikeId];
         bike.status = BikeStatus.Available;
         emit ReturnBike(msg.sender, bikeId);
     }
-
-    //报告损坏
+    
+    /**
+     * 报告损坏
+     * @param bikeId 共享单车ID
+     */
     function reportDamge(uint256 bikeId) external  userRegistered(msg.sender) requireStatus(bikeId, BikeStatus.Available){
-        //修改自行车状态
         Bike storage bike = _bikes[bikeId];
         bike.status = BikeStatus.InRepair;
         emit ReportDamage(msg.sender, bikeId);
     }
 
-    //修理损坏
+    /**
+     * 修复损坏
+     * @param bikeId 共享单车ID
+     */
     function fixDamge(uint256 bikeId) external onlyAdmin requireStatus(bikeId, BikeStatus.InRepair){
-        //修改自行车状态
         Bike storage bike = _bikes[bikeId];
-        bike.status = BikeStatus.Available;
-        emit FixDamage(bikeId);
+        bike.status = BikeStatus.InRepair;
     }
-    
-    //奖励
+
+    /**
+     * 奖励用户 
+     * @param user 用户地址
+     * @param credits 奖励积分数量
+     */
     function reward(address user, uint32 credits) external userRegistered(user) onlyAdmin{
-        //调用addCredits方法
         userStorage.addCredits(user, credits);
         emit Reward(user, credits);
     }
 
-    //惩罚
+    /**
+     * 惩罚用户
+     * @param user 用户地址
+     * @param credits 惩罚积分数量
+     */
     function punish(address user, uint32 credits) external userRegistered(user) onlyAdmin{
-        //调用subCredits
         userStorage.subCredits(user, credits);
         emit Punish(user, credits);
     }
 
-    //设置信誉分阈值
+    /**
+     * 设置用户信用积分阈值
+     * @param newThreshold 新的阈值
+     */
     function setCreditThreshold(uint32 newThreshold) external onlyAdmin {
         userCreditThreshold = newThreshold;
         emit SetCreditThreshold(newThreshold);
     }
 
-    //转让管理员
+    /**
+     * 权限转让
+     */
     function transferAdmin(address newAdmin) external onlyAdmin {
         address oldAdmin = admin;
         admin = newAdmin;
         emit TransferAdmin(oldAdmin, newAdmin);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
--- contracts/business_template/shared_bike/UserStorage.sol
+++ contracts/business_template/shared_bike/UserStorage.sol
@@ -1,76 +1,94 @@
 pragma solidity ^0.6.10;
 
+// 用户数据存储合约
 contract UserStorage{
 
+    // 合约调用者
     address public admin;
-    
 
+    // 用户实体类
     struct User {
-        string name;            //姓名
-        string contact;         //联系方式
-        uint32 creditPoints;    //信誉分
-        uint32 status;          //用户状态
-    }//结构体，相当于User模板
-
-    User public users;//用户信息对象
-
-    //其中mapping对象储存用户信息，用户信息主要封装在这里
-    mapping(address=>User) public registeredUsers;
-
-    //直接用调用者作为admin地址
-    constructor() public{
-        admin = msg.sender;
+        string name;            // 用户名
+        string contact;         // 联系方式
+        uint32 creditPoints;    // 信用积分
+        uint32 status;          // 用户状态
     }
-   
+
+    // 用于存储用户的信息
+    mapping(address=>User) private registeredUsers;
+
+    // 初始化合约的调用者地址
+    constructor(address _admin) public{
+        admin = _admin;
+    }
+
+    // 修饰符函数 只有合约调用者地址才有权限
+    modifier onlyAdmin(){
+        require(msg.sender == admin, "admin required");
+        _;
+    }
+    // 用户注册事件
     event RegisterUser(address indexed addr, string name, string contact);
 
-    //注册
+    /**
+     * 用户注册
+     * @param name 用户名称
+     * @param contact 用户联系方式
+     */
     function registerUser(string memory name, string memory contact) external {
-        //if status==0,is new User
+    	// 判断当前用户是否已经注册
         require(!exists(msg.sender), "user existed");
-       
-        registeredUsers[msg.sender].name = name;
-        registeredUsers[msg.sender].contact = contact;
-        registeredUsers[msg.sender].status = 1;
-        registeredUsers[msg.sender].creditPoints = 100;
-        
+        // 用户注册
+        User storage user = registeredUsers[msg.sender];
+        user.name = name;
+        user.contact = contact;
+        // 触发用户注册事件
         emit RegisterUser(msg.sender, name, contact);
     }
 
-    //查询信誉分
+    /**
+     * 查询用户积分
+     * @param user      用户的地址
+     * @return credits  用户的积分
+     */
     function getCredits(address user) external view returns(uint32 credits) { 
+     	// 判断当前用户是否已经注册
         require(exists(user), "user not exists");       
         return registeredUsers[user].creditPoints;
     }
 
-    //用户是否注册过
+    /**
+     * 查询当前用户是否已经注册
+     */
     function exists(address user) public view returns(bool) {
-        //用户状态为0就是未注册
         return registeredUsers[user].status != 0;
     }
 
-    //加分
-    function addCredits(address user, uint32 credits) public  {
+    /**
+     * 添加积分
+     * @param user 用户地址
+     * @param credits 积分的数量
+     */
+    function addCredits(address user, uint32 credits) public onlyAdmin {
+    	// 积分必须要大于0 用户需要已经注册
         require(credits > 0, "credits zero");
-        require(exists(user), "user not exists");  
-         
-        uint32 oldPoint=registeredUsers[user].creditPoints;
-        
+        require(exists(user), "user not exists");   
         uint32 newPoint = registeredUsers[user].creditPoints + credits;
-        require(newPoint > oldPoint, "overflow");
+        require(newPoint > credits, "overflow");
         registeredUsers[user].creditPoints= newPoint;
     }
 
-    //减分
-    function subCredits(address user, uint32 credits) public  {
+    /**
+     * 减少积分操作
+     * @param user 用户地址
+     * @param credits 积分的数量
+     */
+    function subCredits(address user, uint32 credits) public onlyAdmin {
+    	// 积分必须要大于0 用户需要已经注册
         require(credits > 0, "credits zero");
         require(exists(user), "user not exists");   
-
-        uint32 oldPoint=registeredUsers[user].creditPoints;
-        require(credits <= oldPoint,"overflow");
         uint32 newPoint = registeredUsers[user].creditPoints - credits;
-        require(newPoint < oldPoint, "overflow");
-
+        require(newPoint < credits, "overflow");
         registeredUsers[user].creditPoints= newPoint;
     }
 }
